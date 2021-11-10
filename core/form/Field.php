e43f12b2e9331216c96095b1fab8228a62c4e43f
Fabien Winkler <fabien.winkler@outlook.fr>
2021-11-10 23:53:25 +0100

    [Feat] generate field dynamiccaly

--- core/form/Field.php
+++ core/form/Field.php
@@ -6,22 +6,57 @@ use app\core\Model;
 
 class Field
 {
+    public const TYPE_TEXT = 'text';
+    public const TYPE_EMAIL = 'email';
+    public const TYPE_PASSWORD = 'password';
+
+    public string $type;
     public Model $model;
     public string $attribute;
 
     public function __construct(Model $model, string $attribute)
     {
+        $this->type = '';
         $this->model = $model;
         $this->attribute = $attribute;
     }
 
+    /**
+     * Generate input text field
+     * 
+     * @return string
+     */
     public function __toString()
     {
-        return sprintf('
+        return sprintf(
+            '
             <div class="form-floating mb-3">
-            <input type="text" name="%s" value="%s" class="form-control%s">
-            <label>%s</label>
-        </div>
-        ', $this->attribute, $this->model->{$this->attribute} , ' is-valid', $this->attribute);
+                <input type="%s" name="%s" value="%s" placeholder="%s" class="form-control%s">
+                <label>%s</label>
+                <div class="invalid-feedback">
+                    %s
+                </div>
+            </div>
+            ',
+            $this->type,
+            $this->attribute,
+            $this->model->{$this->attribute},
+            $this->attribute,
+            $this->model->hasError($this->attribute) ? ' is-invalid' : '',
+            $this->attribute,
+            $this->model->getFirstError($this->attribute),
+        );
+    }
+
+    /**
+     * Generate input password field
+     * 
+     * @return string
+     */
+    public function passwordField()
+    {
+        $this->type = self::TYPE_PASSWORD;
+
+        return $this;
     }
 }
--- views/register.php
+++ views/register.php
@@ -1,42 +1,21 @@
 <h1 class="mb-3">Create an account</h1>
 
 <?php $form = app\core\form\Form::begin('', 'post') ?>
-    <?= $form->field($model, 'firstname') ?>
-    <?= $form->field($model, 'lastname') ?>
+<div class="row">
+    <div class="col">
+        <?= $form->field($model, 'firstname') ?>
+    </div>
+    <div class="col">
+        <?= $form->field($model, 'lastname') ?>
+    </div>
+</div>
+
     <?= $form->field($model, 'email') ?>
-    <?= $form->field($model, 'password') ?>
-    <?= $form->field($model, 'passwordConfirm') ?>
 
-    <button type="submit" class="btn btn-primary">Register</button>
+    <?= $form->field($model, 'password')->passwordField() ?>
+
+    <?= $form->field($model, 'passwordConfirm')->passwordField() ?>
+
+
+<button type="submit" class="btn btn-primary">Register</button>
 <?= app\core\form\Form::end() ?>
-
-<!-- <div class="row">
-    <div class="col">
-        <div class="form-floating mb-3">
-            <input type="text" name="firstname" value="<?php echo $model->firstname ?? '' ?>" class="form-control <?= $model->hasError('firstname') ? 'is-invalid' : '' ?>">
-            <label>Firstname</label>
-            <div class="invalid-feedback">
-                <?php //echo $model->getFirstError('firstname') ?>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="form-floating mb-3">
-            <input type="text" name="lastname" class="form-control">
-            <label>Lastname</label>
-        </div>
-
-    </div>
-</div>
-<div class="form-floating mb-3">
-    <input type="email" name="email" class="form-control">
-    <label>Email address</label>
-</div>
-<div class="form-floating mb-3">
-    <input type="password" name="password" class="form-control">
-    <label>Password</label>
-</div>
-<div class="form-floating mb-3">
-    <input type="password" name="passwordConfirm" class="form-control">
-    <label>Password confirm</label>
-</div> -->
